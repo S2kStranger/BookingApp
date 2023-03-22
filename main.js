6c44ffe0c0c025174cb28074840f1bf668441bd6
S2kStranger <s2kkasera@gmail.com>
2023-03-22 23:56:02 +0530

    Active delete button
    Added delete button which remove user data from list and local storage

--- main.js
+++ main.js
@@ -9,21 +9,44 @@ function add_data(e)
     var phone_no=document.getElementById('phone_no').value;
     var str=name+" - "+email+" - "+phone_no;
 
+    const user_obj=new newUser(name,email,phone_no);
+
+    var obj_serialized=JSON.stringify(user_obj);
+    localStorage.setItem(user_obj.email,obj_serialized);
     //creating li attribute
     var data=document.getElementById('users');
     var li=document.createElement('li');
     li.appendChild(document.createTextNode(str));
+
+    //create delete button
+    var btn=document.createElement('button');
+    btn.appendChild(document.createTextNode('Delete'));
+    li.append(btn);
+    btn.addEventListener('click',(e) =>
+    {
+        e.preventDefault();
+        li.remove();
+        localStorage.removeItem(user_obj.email);
+    });
     data.appendChild(li);
 
     //adding data as an object to local storage
 
-    const user_obj={
+   /* const user_obj={
         name: name,
         email: email,
         phone_no: phone_no
     };
+    */
 
-    var obj_serialized=JSON.stringify(user_obj);
-    localStorage.setItem(email,obj_serialized);
+    
 
+}
+
+
+function newUser(name,email,phone_no)
+{
+    this.name=name;
+    this.email=email;
+    this.phone_no=phone_no;
 }
