9812f26b69b921078283adb7072ca62e9ae797de
S2kStranger <s2kkasera@gmail.com>
2023-03-23 21:52:01 +0530

    Edit button added
    Edit button allows to edit the user details

--- main.js
+++ main.js
@@ -4,15 +4,19 @@ myform.addEventListener('submit',add_data);
 function add_data(e)
 {
     e.preventDefault();
-    var name=document.getElementById('name').value;
-    var  email=document.getElementById('email').value;
-    var phone_no=document.getElementById('phone_no').value;
-    var str=name+" - "+email+" - "+phone_no;
+    var name=document.getElementById('name');
+    var  email=document.getElementById('email');
+    var phone_no=document.getElementById('phone_no');
+    var str=name.value+" - "+email.value+" - "+phone_no.value;
 
-    const user_obj=new newUser(name,email,phone_no);
+    const user_obj=new newUser(name.value,email.value,phone_no.value);
 
     var obj_serialized=JSON.stringify(user_obj);
     localStorage.setItem(user_obj.email,obj_serialized);
+
+    name.value='';
+    email.value='';
+    phone_no.value='';
     //creating li attribute
     var data=document.getElementById('users');
     var li=document.createElement('li');
@@ -28,6 +32,23 @@ function add_data(e)
         li.remove();
         localStorage.removeItem(user_obj.email);
     });
+
+    //create edit button
+    var editbtn=document.createElement('button');
+    editbtn.appendChild(document.createTextNode('Edit'));
+    li.append(editbtn);
+    editbtn.addEventListener('click',(e)=>
+    {
+        e.preventDefault();
+        li.remove();
+        localStorage.removeItem(user_obj.email);
+        name.value=user_obj.name;
+        email.value=user_obj.email;
+        phone_no.value=user_obj.phone_no;
+
+    });
+
+
     data.appendChild(li);
 
     //adding data as an object to local storage
